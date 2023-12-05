8e5c8a39ee5668d60a3c0a53145988b5940e1c1f
BOMMAKAPARANDHAMAREDDY <parandhama.b@kalvium.community>
2023-12-05 16:28:11 +0530

    game development 1 completed

---
+++ app.js
@@ -0,0 +1,11 @@
+// Iteration 1: Make the Play button functional.
+// Description: When the Play button is clicked the game page should be displayed.
+
+const playButton = document.getElementById("play-button");
+
+const scoreBoard = document.getElementById("score-board");
+
+playButton.onclick = () => {
+
+    window.location.href = "./game.html";
+};
